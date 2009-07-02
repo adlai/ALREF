41ceda1520e4d56afb8226791f15d8b37dc6371a
Adlai <munchking@gmail.com>
2009-07-02 20:28:50 +0300

    More user-friendly
    It is now possible to customize the default :key and :test args.
    
    Also, when a :key argument is given to #'(setf alref), it will
    issue a continuable error when a new pair is about to be created.
    Previously, the pair would be created silently, introducing potential
    bugs to a program.
    
    Future versions of ALREF will probably include support for some
    amount of 're-structuring' in this case.

--- alref.asd
+++ alref.asd
@@ -6,7 +6,7 @@
 
 (defsystem alref
   :name "ALREF Utility"
-  :version "2.1"
+  :version "2.2"
   :maintainer "Adlai"
   :author "Adlai"
   :license "MIT-style; See README"
@@ -14,6 +14,6 @@
   :long-description
   "This package exports the ALREF symbol, along with two bits of
 behavior: a function #'alref, and a setf expansion. These work
-quite intuitively. See README for documentation."
+quite intuitively. See README for documentation and options."
   :serial T
   :components ((:file "alref")))
--- alref.lisp
+++ alref.lisp
@@ -2,14 +2,19 @@
   (:use :cl)
   (:export :alref))
 (in-package :alref)
+(defvar *default-test* #.#'eql)
+(defvar *default-key* #.#'identity)
 (defmacro with-gensyms (vars &body body)
   `(let ,(loop for x in vars collect `(,x (gensym)))
      ,@body))
-(defun alref (item alist &key (test #'eql) (key #'identity))
+(defun alref (item alist &key
+              (test *default-test*)
+              (key *default-key*))
   "Retreive the value corresponding to ITEM in ALIST."
   (cdr (assoc item alist :test test :key key)))
 (define-setf-expander alref (item alist
-                             &key (test #'eql) (key #'identity)
+                             &key (test *default-test*)
+                             (key *default-key*)
                              &environment env)
   "Set the value corresponding to ITEM in ALIST."
   (multiple-value-bind (foo foo stores setter)
@@ -31,5 +36,17 @@
                      (,it (setf (cdr ,it) ,new))
                      (T (let ((,(car stores)
                                (acons ,g-item ,new ,g-alist)))
+                          (unless (eq ,g-key #'identity)
+                            (cerror "Add the key/value pair anyways."
+                                    "~&#'(setf alref) was provided a ~
+                              :key argument. This is ignored~%  when ~
+                              creating a new pair.~%~@
+                                 If this pair is added, it will not ~
+                              be accessible to #'alref~%  using this ~
+                              :key, and could incroduce bugs to your ~
+                              program.~%~@
+                                Key provided: ~S~@
+                                New pair: ~S"
+                                    ,g-key (car ,(car stores))))
                           ,setter)))
               `(cdr ,it)))))
